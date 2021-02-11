1262cede1bd712a27f96a90303651c82fbeab2a6
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-02-11 10:28:13 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,7 @@
+for(int i =0; i > 2; i++){
+  //Indent
+  //We are creating a variable called i in this for loop. In Python, it would look like this: for i in range(2):
+  //This is where we are gonna put the code
+  System.out.println('hi');
+  //This is going to print out 'hi' two times. i++ is just i + 1
+  //Change the i > 2 to any number you would like
